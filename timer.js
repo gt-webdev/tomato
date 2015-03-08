47086837c73f83968b01beb10094ba50ece18b15
Jennifer Hwang <jmhwang7@gmail.com>
2015-03-08 13:36:56 -0400

    Fixed bug by trigerring timer start with a button instead of when the timer page loads.

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 function init() {
+	addOnClick();
 	addMessageListeners();
-	startTimer();
 }
 
 function startTimer() {
@@ -22,6 +22,12 @@ function addMessageListeners() {
 				break;
 		}
 	});
+}
+
+function addOnClick() {
+	document.getElementById("start").onclick = function() {
+		startTimer();
+	}
 }
 
 document.addEventListener('DOMContentLoaded', init);
