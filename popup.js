7c553615fb7092fbd57be7732d953ffcee263c8a
Jennifer Hwang <jmhwang7@gmail.com>
2015-03-09 13:14:18 -0400

    Added sentence to indicate current settings in initial page.

--- popup.js
+++ popup.js
@@ -2,6 +2,9 @@
  * Initially called when extension page loads. Attaches click handlers to all the buttons.
  */
 function init() {
+	document.getElementById("pomodoro").innerText = localStorage["pomodoro-selection"] || 10;
+	document.getElementById("break").innerText = localStorage["break-selection"] || 10;
+
 	var buttonGroups = document.getElementsByClassName("time-buttons-group")
 	Array.prototype.forEach.call(buttonGroups, function(divElem) {
 
@@ -21,6 +24,7 @@ function timeButtonOnClickHandler(event) {
 	var timeSelected = +targetElem.innerText; // Get button text and convert to number
 	var settingKey = targetElem.parentNode.id; // Get id of node (tells us whether break or pomodoro)
 	localStorage[settingKey] = timeSelected; // Save in localstorage.
+	document.getElementById(settingKey.split("-")[0]).innerText = timeSelected;
 }
 
 document.addEventListener('DOMContentLoaded', init);
